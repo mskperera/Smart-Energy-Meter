402fbed592c5436a7098f02dc1c0840939d1eafb
Susantha Perera <33259705+mskperera@users.noreply.github.com>
2024-02-27 11:51:54 +0530

    Create user.js

---
+++ src/action/user.js
@@ -0,0 +1,79 @@
+import customAxios from "../utils/axios";
+
+
+export const  deleteUser= async (userId) => {
+  try {
+    return await customAxios
+      .delete(`/deleteUser/${userId}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return err.response;
+      });
+  } catch (err) {
+    return err;
+  }
+}
+
+export const getUserbyUserId= async (userId) => {
+  try {
+    return await customAxios
+      .get(`/getUserbyUserId/${userId}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return err.response;
+      });
+  } catch (err) {
+    return err;
+  }
+}
+
+export const getUsers= async () => {
+  try {
+    return await customAxios
+      .get(`/getUsers`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return err.response;
+      });
+  } catch (err) {
+    return err;
+  }
+}
+
+
+export const addUser= async (payload) => {
+  try {
+    return await customAxios
+      .post(`/addUser`,payload, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => {
+        return res;
+      })
+      .catch((err) => {
+        return err.response;
+      });
+  } catch (err) {
+    return err;
+  }
+}
