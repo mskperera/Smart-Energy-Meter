1f9e71559a045f1fe4a54a188e0167a31bae5e67
Susantha Perera <33259705+mskperera@users.noreply.github.com>
2024-07-12 16:20:36 +0530

    Update serviceProfile.js

--- src/action/serviceProfile.js
+++ src/action/serviceProfile.js
@@ -41,3 +41,27 @@ export const serviceProfileSetup= async (payload) => {
     return err;
   }
 }
+// payload={
+
+//   "userName":"hhhoooo5",
+//       "displayName":"UserHoooo",
+//   "password":"2211",
+//   "isActive":true,
+//   "email":"sswo@gmail.com",
+//   "mobileNo":"054434",
+//   "profilePic":"1",
+  
+//       "siteAddress":"siteaa23",
+//       "tel":"0227872837",
+//       "deviceId":39,
+//       "deviceName":"device 39 n",
+//       "supplierId":1,
+//       "consumerCategoryid":3,
+//       "consumerSubCategoryId":7,
+//       "billingSessionStart":"2024-06-18",
+//       "billingSessionEnd":"2024-07-18",
+//       "measuringModeId":1,
+//       "budgetedValue":1200,
+//       "opertationalMetricId":1
+
+//   }
