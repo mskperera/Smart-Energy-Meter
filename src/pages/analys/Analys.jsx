54492111bbbbc9317e29e6d2041089b4eb5ec216
Lasitha Gimhan <lasithagimhan99@gmail.com>
2025-02-06 16:48:38 +0530

    connect device list

--- src/pages/analys/Analys.jsx
+++ src/pages/analys/Analys.jsx
@@ -1,9 +1,105 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, ResponsiveContainer, Tooltip  } from 'recharts';
 import './Analys.css';
 import BottomNav from '../../components/bottommenu/BottomNav';
+import { getDevicesByUserId } from '../../action/device';
+import { getBillingSessionDateRangeBySessionStartDateTimebasedTable } from '../../action/billingSession';
+import { useSelector } from 'react-redux';
+
 
 const Analys = () => {
+
+   const userData=JSON.parse(localStorage.getItem('userData'));
+   const [deviceNames, setDeviceNames] = useState([]);
+   const [peakTime, setPeakTime] = useState([]);
+   const [offPeakTime, setOffPeakTime] = useState([]);
+   const [dayTime, setDayTime] = useState([]);
+   
+   const  [totalSum, setTotalSum] = useState([]);
+   const selectedDevice = useSelector((state) => state.device.selectedDevice);
+
+    const loadDevicesByUserId = async () => {
+   
+     const result = await getDevicesByUserId(userData.userId);
+     // console.log('deviceDetails', result);
+     
+     if (result.status === 200) {
+       const devices = result.data.map(device => ({ id: device.deviceId, deviceTypeId: device.deviceTypeId, name: device.deviceName }));
+       // console.log('devices12123313',devices);
+        setDeviceNames(devices);
+      //dispatch(setDropDevices({dropDeviceList:devices}));
+      //dispatch(setSelectedDevie({ device: devices[0]}));
+      //setSelectedDeviceName(devices[0]?.name)
+     }
+     
+   }
+     useEffect(() => {
+       loadDevicesByUserId();
+     }, []); 
+
+      useEffect(() => {
+         if (selectedDevice) {
+           loadBillingSessionDateRangeBySessionStartDateTimebasedTable();
+         }
+       }, [selectedDevice]);
+
+     const loadBillingSessionDateRangeBySessionStartDateTimebasedTable = async () => {
+           try {
+             // setLoadData(true);
+             const payload = {
+               deviceId: selectedDevice.id,
+             };
+             const res = await getBillingSessionDateRangeBySessionStartDateTimebasedTable(payload);
+             
+             console.log('getbilling-----SessionByDeviceId',res)
+     
+             console.log('getbillingSessionByDeviceId',res.data);
+     
+               const usage = res.data[1];
+               const totalSum = usage.TotalKwh + usage.TotalKwh2 + usage.TotalKwh3;
+               setDayTime(totalSum);
+               
+               const usage1 = res.data[2];
+               const totalSum1 = usage1.TotalKwh + usage1.TotalKwh2 + usage1.TotalKwh3;
+               setOffPeakTime(totalSum1);
+     
+               const usage2 = res.data[0];
+               const totalSum2 = usage2.TotalKwh + usage2.TotalKwh2 + usage2.TotalKwh3;
+               setPeakTime(totalSum2);
+     
+               const totalSum3 = totalSum + totalSum1 + totalSum2; 
+               setTotalSum(totalSum3);
+               
+     
+             console.log('sumpeak',totalSum);
+
+             console.log('getbillingSessionByDeviceId',res)
+            //  if (res.data.length > 0) {
+            //    const sessions = res.data.reduce((acc, session) => {
+            //      const startDate = new Date(session.startDate);
+            //      const endDate = new Date(session.endDate);
+            //      acc[session.deviceBillingSessionId] = {
+            //        startDate,
+            //        endDate,
+            //      };
+            //      setSelectedTimes((prevTimes) => ({
+            //        ...prevTimes,
+            //        [session.deviceBillingSessionId]: {
+            //          startTime: startDate.toTimeString().slice(0, 5),
+            //          endTime: endDate.toTimeString().slice(0, 5),
+            //        },
+            //      }));
+            //      return acc;
+            //    }, {});
+            //    //setBillingSession(res.data);
+            //    setSelectedDates(sessions);
+            //  }
+             // setLoadData(false);
+           } catch (error) {
+             console.error('Error fetching billing session:', error);
+             // setLoadData(false);
+           }
+         };
 
   const [clickedPoint, setClickedPoint] = useState(null);
   
@@ -47,18 +143,25 @@ const Analys = () => {
       <div className="dashboard-container">
         {/* Left Panel */}
         <div className="left-panel" style={{ backgroundColor: '#2d2e2e' }}>
-          <div className="group-card">
+          {/* <div className="group-card">
             <div className="group-title">Group 1</div>
             <div className="group-energy">{totalEnergy} kWh</div>
             <div className="group-stats">29.5kw 149.45A</div>
-          </div>
+          </div> */}
           
-          {devices.map((device, index) => (
+          {deviceNames.map((device, index) => (
             <div key={device.id} className={`device-card device-${index + 1}`}>
               <div className="device-content">
                 <div className="device-info">
                   <div className="">{device.name}</div>
-                  <div className="device-energy">{device.energy}kWh</div>
+                  <div className="device-energy">Total:&nbsp;
+                       {
+                          totalSum.toLocaleString(undefined, {
+                            minimumFractionDigits: 2,
+                            maximumFractionDigits: 2,
+                          })
+                       } kWh</div>
+                  
                   <div className="device-energy">{device.power}</div>
                 </div>
                 <div className="device-stats">
@@ -99,7 +202,7 @@ const Analys = () => {
             {Object.keys(chartKeys).map((title, index) => (
               <div key={title} className="chart-card">
                 <div className="chart-title">{title}</div>
-                <ResponsiveContainer width="100%" height="80%">
+                {/* <ResponsiveContainer width="100%" height="80%">
                   <LineChart data={chartData} onClick={handlePointClick} style={{ margin: '0 auto' }}>
                     <CartesianGrid strokeDasharray="3 3" />
                     <XAxis dataKey="day" />
@@ -118,7 +221,7 @@ const Analys = () => {
                       strokeWidth={2}
                     />
                   </LineChart>
-                </ResponsiveContainer>
+                </ResponsiveContainer> */}
               </div>
             ))}
           </div>
